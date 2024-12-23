7a67c31646d922a35b3b2b52462db71e76ef0d26
Toby Fey <tobyfey@protonmail.com>
2024-12-23 13:33:05 -0500

    update version number

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
     return out
 
 setup(name='docassemble.VT813',
-      version='1.0',
+      version='1.1',
       description=('Financial Affidavit - Vermont - Form 813A'),
       long_description='# docassemble.VtFeeWaiver\r\n\r\nFinancial Affidavit - Vermont - Form 813A\r\n\r\n## Author\r\n\r\nGuided interview by Legal Services Vermont / VTLawHelp.org website\r\n\r\n',
       long_description_content_type='text/markdown',
